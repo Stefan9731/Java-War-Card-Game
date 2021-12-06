09ef251fb689756226f1e6b86fed24e7ddff697d
Stefan Ignat <57329004+Stefan9731@users.noreply.github.com>
2021-12-06 16:40:22 -0500

    Fixed Face card values
    Originally Ace was 11, Jack 12, etc. Moved Ace to 14, Jack to 11, ect.

--- Java Card Game/Card.java
+++ Java Card Game/Card.java
@@ -5,10 +5,10 @@ public class Card{
         this.rank = rank;
     }
     public int getValue() {
-        if (this.rank == "A") return 11;
-        else if (this.rank == "J") return 12;
-        else if (this.rank == "Q") return 13;
-        else if (this.rank == "K") return 14;
+        if (this.rank == "J") return 11;
+        else if (this.rank == "Q") return 12;
+        else if (this.rank == "K") return 13;
+        else if (this.rank == "A") return 14;
         return Integer.parseUnsignedInt(this.rank);
     }
 }
